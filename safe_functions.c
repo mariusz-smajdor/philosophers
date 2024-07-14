1fde37f3829dc0675ec8b466b4cfb461c8a92ca6
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-07-14 18:03:11 +0000

    add safe_malloc function

---
+++ safe_functions.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   safe_functions.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msmajdor <msmajdor@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/14 20:34:47 by msmajdor          #+#    #+#             */
+/*   Updated: 2024/07/14 20:37:20 by msmajdor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	*safe_malloc(size_t size)
+{
+    void	*ptr;
+
+    ptr = malloc(size);
+    if (!ptr)
+        error_exit("Memory allocation failed!");
+    return (ptr);
+}
