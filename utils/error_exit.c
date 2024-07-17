9fc51ca63a7aa3209c9b35f77aef3f38eaed2c25
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-07-17 14:48:08 +0000

    move all helper functions into new utils folder

--- utils/error_exit.c
+++ utils/error_exit.c
@@ -1,26 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.c                                            :+:      :+:    :+:   */
+/*   error_exit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msmajdor <msmajdor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/12 21:34:47 by msmajdor          #+#    #+#             */
-/*   Updated: 2024/07/12 21:37:20 by msmajdor         ###   ########.fr       */
+/*   Created: 2024/07/17 21:34:47 by msmajdor          #+#    #+#             */
+/*   Updated: 2024/07/17 21:37:20 by msmajdor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-bool	is_digit(const char c)
-{
-	return (c >= '0' && c <= '9');
-}
-
-bool	is_space(const char c)
-{
-	return (c == ' ' || (c >= '\t' && c <= '\r'));
-}
 
 void	error_exit(const char *msg)
 {
