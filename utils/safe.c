7e39572b8d6e942805738089f139bc43814f2d8f
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-07-18 13:19:49 +0000

    refactor safe functions

--- utils/safe.c
+++ utils/safe.c
@@ -27,13 +27,13 @@ void	safe_thread(pthread_t *thread, void *(*routine)(void *), void *data, t_opco
 	if (opcode == JOIN)
 		if (pthread_join(*thread, NULL) != 0)
 			error_exit("Thread join failed!");
-	else if (opcode == DETACH)
+	if (opcode == DETACH)
 		if (pthread_detach(*thread) != 0)
 			error_exit("Thread detach failed!");
-	else if (opcode == CREATE)
+	if (opcode == CREATE)
 		if (pthread_create(thread, NULL, routine, data) != 0)
 			error_exit("Thread creation failed!");
-	else
+	if (!(opcode >= CREATE && opcode <= DETACH))
 		error_exit("Invalid thread opcode!");
 }
 
@@ -42,15 +42,15 @@ void	safe_mutex(t_mutex *mutex, t_opcode opcode)
 	if (opcode == INIT)
 		if (pthread_mutex_init(mutex, NULL) != 0)
 			error_exit("Mutex initialization failed!");
-	else if (opcode == DESTROY)
+	if (opcode == DESTROY)
 		if (pthread_mutex_destroy(mutex) != 0)
 			error_exit("Mutex destruction failed!");
-	else if (opcode == LOCK)
+	if (opcode == LOCK)
 		if (pthread_mutex_lock(mutex) != 0)
 			error_exit("Mutex lock failed!");
-	else if (opcode == UNLOCK)
+	if (opcode == UNLOCK)
 		if (pthread_mutex_unlock(mutex) != 0)
 			error_exit("Mutex unlock failed!");
-	else
+	if (!(opcode >= INIT && opcode <= UNLOCK))
 		error_exit("Invalid mutex opcode!");
 }
